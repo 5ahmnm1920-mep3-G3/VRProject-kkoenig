6f0cc6e7efee1a54eddcad952fa671c767765418
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2020-03-25 15:24:01 +0100

    Added GameOver Script, so the game ends if you loose or win

---
+++ Assets/MyGame/Scripts/GameOverScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+    Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<Text>();
+        txt.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreScript.scoreValue >= 50)
+        {
+           txt.text = "CONGRATULATION! YOU SAVED THE WORLD";
+           txt.enabled = true;
+        }
+
+        else if (ScoreScript.scoreValue == -50)
+        {
+            txt.text = "YOU LOST! DON'T LITTER NATURE";
+            txt.enabled = true;
+        }
+    }
+}
